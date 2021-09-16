a0c07a8a98097b23f2f0add963ef4187262894a2
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-09-16 18:44:07 -0500

    added board reset - also using to set initial board state

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,46 @@
 console.log('script file linked');
 let gameSize = 16;
+let firstChoice = '';
+let secondChoice = '';
 
 let cardColors = [
-    'red',
-    'red',
-    'green',
-    'green',
-    'purple',
-    'purple',
-    'yellow',
-    'yellow',
-    'blue',
-    'blue',
-    'orange',
-    'orange',
-    'brown',
-    'brown',
-    'pink',
-    'pink',
+	'red',
+	'red',
+	'green',
+	'green',
+	'purple',
+	'purple',
+	'yellow',
+	'yellow',
+	'blue',
+	'blue',
+	'orange',
+	'orange',
+	'brown',
+	'brown',
+	'pink',
+	'pink',
 ];
 
 let cardsForGame = shuffle(cardColors);
 console.log(cardsForGame);
 
+//https://bost.ocks.org/mike/shuffle/ - Fisher-Yates Shuffle
+function shuffle(array) {
+	let m = array.length;
+	let t, i;
+	// While there remain elements to shuffle…
+	while (m) {
+		// Pick a remaining element…
+		i = Math.floor(Math.random() * m--);
+
+		// And swap it with the current element.
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
+	return array;
+}
 
 for (i = 0; i < gameSize; i++) {
 	document
@@ -31,30 +49,25 @@ for (i = 0; i < gameSize; i++) {
 		.setAttribute('id', i);
 	document.getElementById(i).setAttribute('class', 'card');
 }
+ 
 
 let cards = document.querySelectorAll('.card');
+resetBoard()
+
 for (i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function selectCard(event) {
-        // event.preventDefault()
-        console.log(`Card ${this.id} clicked`);
-    });
+	cards[i].addEventListener('click', function selectCard(event) {
+		if (this.style.backgroundColor === 'white') {
+            console.log(`Card ${this.id} clicked`);
+		}
+		// event.preventDefault()
+		// console.log(`Card ${this.id} clicked`);
+	});
 }
 
-//https://bost.ocks.org/mike/shuffle/ - Fisher-Yates Shuffle
-function shuffle(array) {
-    let m = array.length;
-    let t, i;
-
-    // While there remain elements to shuffle…
-    while (m) {
-        // Pick a remaining element…
-        i = Math.floor(Math.random() * m--);
-
-        // And swap it with the current element.
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
-    }
-
-    return array;
+function resetBoard() {
+	for (i = 0; i < cards.length; i++) {
+		cards[i].style.backgroundColor = 'white';
+	}
+	firstChoice = '';
+	secondChoice = '';
 }
