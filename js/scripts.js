ca99ddceabf966b0709990089ee3a3b1d6d4fe9f
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-09-16 21:10:53 -0500

    game works after manipulating timeouts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 console.log('script file linked');
 let gameSize = 16;
-const firstChoice = {
-    id: '',
-    color: 'white'   
-}
-const secondChoice = {
-    id: '',
-    color: 'white'   
-}
-let winCon = 1;
+let firstChoice = {
+	id: '',
+	color: 'white',
+};
+let secondChoice = {
+	id: '',
+	color: 'white',
+};
+let winCon = 0;
 
 let cardColors = [
 	'red',
@@ -60,77 +60,80 @@ for (i = 0; i < gameSize; i++) {
 let cards = document.querySelectorAll('.card');
 resetBoard();
 
-for (i = 0; i < cards.length; i++) {
-	cards[i].addEventListener('click', function selectCard(event) {
-		if (this.style.backgroundColor === 'white') {
-			// console.log(`Card ${this.id} clicked`);
-            flipCard(this)
-            setCardCondition(this)
-            checkCards()
-            
-        }
-		// event.preventDefault()
-		// console.log(`Card ${this.id} clicked`);
-	});
-}
+	for (i = 0; i < cards.length; i++) {
+		cards[i].addEventListener('click', function selectCard(event) {
+			if (this.style.backgroundColor === 'white') {
+				// console.log(`Card ${this.id} clicked`);
+				flipCard(this);
+				setCardCondition(this);
+				checkCards();
+			}
+		});
+	}
+
+
 //reset board back to defaults - also used to set initial board state
 function resetBoard() {
 	for (i = 0; i < cards.length; i++) {
 		cards[i].style.backgroundColor = 'white';
 	}
-	resetCards()
+	resetCards();
 }
 //change color on flip
 function flipCard(c) {
-    c.style.backgroundColor = cardsForGame[c.id]
+	c.style.backgroundColor = cardsForGame[c.id];
 }
 
 function setCardCondition(c) {
-    if (firstChoice.id === '') {
-        firstChoice.id = c.id 
-        firstChoice.color = c.style.backgroundColor
-    } else if
-        (secondChoice.id === '') {
-            secondChoice.id = c.id
-            secondChoice.color = c.style.backgroundColor
-        }
-    console.log(`FC = ${firstChoice.id}`)
-    console.log(`FC = ${firstChoice.color}`)
-    console.log(`SC = ${secondChoice.id}`)  
-    console.log(`SC = ${secondChoice.color}`)      
-}        
+	if (firstChoice.id === '') {
+		firstChoice.id = c.id;
+		firstChoice.color = c.style.backgroundColor;
+	} else if (secondChoice.id === '') {
+		secondChoice.id = c.id;
+		secondChoice.color = c.style.backgroundColor;
+	}
+	console.log(`FC = ${firstChoice.id}`);
+	console.log(`FC = ${firstChoice.color}`);
+	console.log(`SC = ${secondChoice.id}`);
+	console.log(`SC = ${secondChoice.color}`);
+}
 
 function checkCards() {
-    if (firstChoice.id != '' && secondChoice.id != '') {
-        if (firstChoice.color === secondChoice.color) {
-            checkWin()
-        } else {
-            cards[firstChoice.id].style.backgroundColor = 'white'
-            cards[secondChoice.id].style.backgroundColor = 'white'
-            resetCards()
-        }
-    }
+	if (firstChoice.id != '' && secondChoice.id != '') {
+		if (firstChoice.color === secondChoice.color) {
+			checkWin();
+		} else {
+			setTimeout(() => {
+				cards[firstChoice.id].style.backgroundColor = 'white';
+				cards[secondChoice.id].style.backgroundColor = 'white';
+				resetCards();
+			}, 200);
+		}
+	}
 }
 
-function checkWin () {
-    for (i=0; i< cards.length; i++) {
-        if (cards[i].style.backgroundColor != 'white') {
-            winCon++
-        }
-    }
-    if (winCon === gameSize) {
-        alert("We have a winner")
-    } else {
-        winCon = 1
-        // cards[firstChoice.id].style.backgroundColor = 'white'
-        // cards[secondChoice.id].style.backgroundColor = 'white'
-        resetCards()
-    }
-    console.log (`WinCon = ${winCon}`)
-} 
-function resetCards() {
-    firstChoice.id = '';
-    firstChoice.color = 'white';
-	secondChoice.id = '';
-    secondChoice.color = 'white';
+function checkWin() {
+	for (i = 0; i < cards.length; i++) {
+		if (cards[i].style.backgroundColor != 'white') {
+			winCon = winCon + 1;
+		}
+	}
+	console.log(`WinCon = ${winCon}`);
+	if (winCon === gameSize) {
+		setTimeout(() => {alert('We have a winner')}, 10)
+	} else {
+		winCon = 0;
+		// cards[firstChoice.id].style.backgroundColor = 'white'
+		// cards[secondChoice.id].style.backgroundColor = 'white'
+		resetCards();
+	}
+	console.log(`WinCon = ${winCon}`);
 }
+function resetCards() {
+	firstChoice.id = '';
+	firstChoice.color = 'white';
+	secondChoice.id = '';
+	secondChoice.color = 'white';
+}
+
+
